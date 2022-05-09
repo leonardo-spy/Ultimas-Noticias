6eedcdb876bc04d8268141414789c1c76594e7e8
leonardo-spy <leonardo.spy@hotmail.com>
2022-05-09 00:35:57 -0300

    teste2

--- main.py
+++ main.py
@@ -3,11 +3,17 @@ from config.config import initialize
 from core.app_temp import app_temp
 from db.fauna import init
 
+from threading import Thread
 from fastapi import FastAPI
 app = FastAPI()
+rodando = False
 
 @app.get("/")
 def read_root():
+    global rodando
+    if rodando != True:
+        rodando = True
+        Thread(target=main).start()
     return {"message": "Server is up and running!"}
 
 def main():
@@ -17,5 +23,3 @@ def main():
     app_temp(vars_env)
 
 
-if __name__ == '__main__':
-    main()
