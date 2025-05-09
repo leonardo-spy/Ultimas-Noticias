85b59c4959e05fa142706001c27e6dea416c8d6c
Leonardo Marques <leonardo.spy@hotmail.com>
2025-05-09 14:59:23 -0300

    fix query fauna

--- controller/database.py
+++ controller/database.py
@@ -4,178 +4,90 @@ from db.fauna import get_client
 def get_noticias(portal_noticia):
     noticias = []
     client = get_client()
-    if portal_noticia == 'todos':
-        result_raw = client.query(q.map_(q.lambda_("ref", q.get(q.var("ref"))),
-            q.paginate(q.reverse(
-                q.union(
-                    q.join(q.documents(q.collection("estadao")),
-                    q.lambda_("ref", q.singleton(q.var("ref")))
-                    ),
-                    q.join(q.documents(q.collection("extra")),
-                    q.lambda_("ref", q.singleton(q.var("ref")))
-                    ),
-                    q.join(q.documents(q.collection("folha")),
-                    q.lambda_("ref", q.singleton(q.var("ref")))
-                    ),
-                    q.join(q.documents(q.collection("g1")),
-                    q.lambda_("ref", q.singleton(q.var("ref")))
-                    ),
-                    q.join(q.documents(q.collection("oglobo")),
-                    q.lambda_("ref", q.singleton(q.var("ref")))
-                    ),
-                    q.join(q.documents(q.collection("uol")),
-                    q.lambda_("ref", q.singleton(q.var("ref")))
-                    ),
-            )),size=50 )
+
+    try:
+        if portal_noticia == 'todos':
+            colecoes = ['estadao', 'extra', 'folha', 'g1', 'oglobo', 'uol']
+            union_list = [q.documents(q.collection(c)) for c in colecoes]
+
+            result_raw = client.query(
+                q.map_(
+                    q.lambda_("ref", q.get(q.var("ref"))),
+                    q.paginate(q.reverse(q.union(*union_list)), size=50)
+                )
             )
+        elif portal_noticia in ['estadao', 'extra', 'folha', 'g1', 'oglobo', 'uol']:
+            result_raw = client.query(
+                q.map_(
+                    q.lambda_("ref", q.get(q.var("ref"))),
+                    q.paginate(q.reverse(q.documents(q.collection(portal_noticia))), size=50)
+                )
             )
-    elif portal_noticia == 'estadao':
-        result_raw = client.query(q.map_(q.lambda_("ref", q.get(q.var("ref"))),
-            q.paginate(q.reverse(
-                q.union(
-                    q.join(q.documents(q.collection("estadao")),
-                    q.lambda_("ref", q.singleton(q.var("ref")))
-                    ),
-            )),size=50 )
-            )
-            )
-    elif portal_noticia == 'extra':
-        result_raw = client.query(q.map_(q.lambda_("ref", q.get(q.var("ref"))),
-            q.paginate(q.reverse(
-                q.union(
-                    q.join(q.documents(q.collection("extra")),
-                    q.lambda_("ref", q.singleton(q.var("ref")))
-                    ),
-            )),size=50 )
-            )
-            )
-    elif portal_noticia == 'folha':
-        result_raw = client.query(q.map_(q.lambda_("ref", q.get(q.var("ref"))),
-            q.paginate(q.reverse(
-                q.union(
-                    q.join(q.documents(q.collection("folha")),
-                    q.lambda_("ref", q.singleton(q.var("ref")))
-                    ),
-            )),size=50 )
-            )
-            )
-    elif portal_noticia == 'g1':
-        result_raw = client.query(q.map_(q.lambda_("ref", q.get(q.var("ref"))),
-            q.paginate(q.reverse(
-                q.union(
-                    q.join(q.documents(q.collection("g1")),
-                    q.lambda_("ref", q.singleton(q.var("ref")))
-                    ),
-            )),size=50 )
-            )
-            )
-    elif portal_noticia == 'oglobo':
-        result_raw = client.query(q.map_(q.lambda_("ref", q.get(q.var("ref"))),
-            q.paginate(q.reverse(
-                q.union(
-                    q.join(q.documents(q.collection("oglobo")),
-                    q.lambda_("ref", q.singleton(q.var("ref")))
-                    ),
-            )),size=50 )
-            )
-            )
-    elif portal_noticia == 'uol':
-        result_raw = client.query(q.map_(q.lambda_("ref", q.get(q.var("ref"))),
-            q.paginate(q.reverse(
-                q.union(
-                    q.join(q.documents(q.collection("uol")),
-                    q.lambda_("ref", q.singleton(q.var("ref")))
-                    ),
-            )),size=50 )
-            )
-            )
-    else:
+        else:
+            return noticias
+    except Exception as e:
+        print(f"Erro ao consultar FaunaDB: {e}")
         return noticias
-    
+
     for noticia in result_raw['data']:
         portal = noticia['ref'].value['collection'].value['id']
+        data_fields = noticia.get('data', {})
+
         if portal == 'estadao':
-            link = titulo = body_text = credito = data = imagem_link = None
-            if 'link' in noticia['data']:
-                link = noticia['data']['link']
-            if 'titulo' in noticia['data']:
-                titulo = noticia['data']['titulo']
-            if 'body_text' in noticia['data']:
-                body_text = noticia['data']['body_text']
-            if 'credito' in noticia['data']:
-                credito = noticia['data']['credito']
-            if 'data' in noticia['data']:
-                data = noticia['data']['data']
-            if 'imagem_link' in noticia['data']:
-                imagem_link = noticia['data']['imagem_link']
-            noticias.append({'portal':portal,'link':link,'titulo':titulo,'body_text':body_text,'credito':credito,'data':data,'imagem_link':imagem_link})
+            noticias.append({
+                'portal': portal,
+                'link': data_fields.get('link'),
+                'titulo': data_fields.get('titulo'),
+                'body_text': data_fields.get('body_text'),
+                'credito': data_fields.get('credito'),
+                'data': data_fields.get('data'),
+                'imagem_link': data_fields.get('imagem_link')
+            })
         elif portal == 'extra':
-            imagem_link = link = body_title = tipo_materia = horario = None
-            if 'imagem_link' in noticia['data']:
-                imagem_link = noticia['data']['imagem_link']
-            if 'link' in noticia['data']:
-                link = noticia['data']['link']
-            if 'body_title' in noticia['data']:
-                body_title = noticia['data']['body_title']
-            if 'tipo_materia' in noticia['data']:
-                tipo_materia = noticia['data']['tipo_materia']
-            if 'horario' in noticia['data']:
-                horario = noticia['data']['horario']
-            noticias.append({'portal':portal,'imagem_link':imagem_link,'link':link,'body_title':body_title,'tipo_materia':tipo_materia,'horario':horario})
+            noticias.append({
+                'portal': portal,
+                'imagem_link': data_fields.get('imagem_link'),
+                'link': data_fields.get('link'),
+                'body_title': data_fields.get('body_title'),
+                'tipo_materia': data_fields.get('tipo_materia'),
+                'horario': data_fields.get('horario')
+            })
         elif portal == 'folha':
-            imagem_link = credito = link = titulo = body_text = data = None
-            if 'imagem_link' in noticia['data']:
-                imagem_link = noticia['data']['imagem_link']
-            if 'credito' in noticia['data']:
-                credito = noticia['data']['credito']
-            if 'link' in noticia['data']:
-                link = noticia['data']['link']
-            if 'titulo' in noticia['data']:
-                titulo = noticia['data']['titulo']
-            if 'body_text' in noticia['data']:
-                body_text = noticia['data']['body_text']
-            if 'data' in noticia['data']:
-                data = noticia['data']['data']
-            noticias.append({'portal':portal,'imagem_link':imagem_link,'credito':credito,'link':link,'titulo':titulo,'body_text':body_text,'data':data})
+            noticias.append({
+                'portal': portal,
+                'imagem_link': data_fields.get('imagem_link'),
+                'credito': data_fields.get('credito'),
+                'link': data_fields.get('link'),
+                'titulo': data_fields.get('titulo'),
+                'body_text': data_fields.get('body_text'),
+                'data': data_fields.get('data')
+            })
         elif portal == 'g1':
-            titulo = link = body_text = imagem_link = horario = local = None
-            if 'titulo' in noticia['data']:
-                titulo = noticia['data']['titulo']
-            if 'link' in noticia['data']:
-                link = noticia['data']['link']
-            if 'body_text' in noticia['data']:
-                body_text = noticia['data']['body_text']
-            if 'imagem_link' in noticia['data']:
-                imagem_link = noticia['data']['imagem_link']
-            if 'horario' in noticia['data']:
-                horario = noticia['data']['horario']
-            if 'local' in noticia['data']:
-                local = noticia['data']['local']
-            noticias.append({'portal':portal,'titulo':titulo,'link':link,'body_text':body_text,'imagem_link':imagem_link,'horario':horario,'local':local})
+            noticias.append({
+                'portal': portal,
+                'titulo': data_fields.get('titulo'),
+                'link': data_fields.get('link'),
+                'body_text': data_fields.get('body_text'),
+                'imagem_link': data_fields.get('imagem_link'),
+                'horario': data_fields.get('horario'),
+                'local': data_fields.get('local')
+            })
         elif portal == 'oglobo':
-            imagem_link = tipo_materia = body_text = link = None
-            if 'imagem_link' in noticia['data']:
-                imagem_link = noticia['data']['imagem_link']
-            if 'tipo_materia' in noticia['data']:
-                tipo_materia = noticia['data']['tipo_materia']
-            if 'body_text' in noticia['data']:
-                body_text = noticia['data']['body_text']
-            if 'link' in noticia['data']:
-                link = noticia['data']['link']
-            noticias.append({'portal':portal,'imagem_link':imagem_link,'tipo_materia':tipo_materia,'body_text':body_text,'link':link})
+            noticias.append({
+                'portal': portal,
+                'imagem_link': data_fields.get('imagem_link'),
+                'tipo_materia': data_fields.get('tipo_materia'),
+                'body_text': data_fields.get('body_text'),
+                'link': data_fields.get('link')
+            })
         elif portal == 'uol':
-            link = imagem_link = tipo_materia = body_text = data = None
-            if 'link' in noticia['data']:
-                link = noticia['data']['link']
-            if 'imagem_link' in noticia['data']:
-                imagem_link = noticia['data']['imagem_link']
-            if 'tipo_materia' in noticia['data']:
-                tipo_materia = noticia['data']['tipo_materia']
-            if 'body_text' in noticia['data']:
-                body_text = noticia['data']['body_text']
-            if 'data' in noticia['data']:
-                data = noticia['data']['data']
-            noticias.append({'portal':portal,'link':link,'imagem_link':imagem_link,'tipo_materia':tipo_materia,'body_text':body_text,'data':data})
-            
+            noticias.append({
+                'portal': portal,
+                'link': data_fields.get('link'),
+                'imagem_link': data_fields.get('imagem_link'),
+                'tipo_materia': data_fields.get('tipo_materia'),
+                'body_text': data_fields.get('body_text'),
+                'data': data_fields.get('data')
+            })
 
     return noticias
